3c6356a2a48f41f511353899860ef450070d13a8
Potapovandrey <potapov@moedelo.org>
2020-02-09 18:03:15 +0300

    PersonDaoQueries.resx

---
+++ src/microservices/lifeevent/PersonDiary.Lifeevent.WebApi/Controllers/LifeEventController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using PersonDiary.Lifeevent.Dto;
+
+
+namespace PersonDiary.React.EFCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LifeEventController : ControllerBase
+    {
+        public LifeEventController()
+        {
+            
+        }
+        
+        [HttpGet]
+        public Task<IActionResult> Get(string json)
+        {
+            throw new NotImplementedException();
+            /*
+            var resp = await new LifeEventModel(factory, mapper).GetItemsAsync(JsonConvert.DeserializeObject<GetLifeEventsDto>(json));
+            if (!Parallel.ForEach(resp.LifeEvents, l =>
+                    {
+                        var responsePerson = new PersonModel(factory, mapper).GetItem(
+                            new Contracts.PersonContract.GetPersonRequest() { Id = l.PersonId, withLifeEvents = false }
+                        );
+                        l.Personfullname = $"{responsePerson.Person.Surname} {responsePerson.Person.Name}";
+                    }
+                ).IsCompleted
+            ) resp.AddMessage(new Contracts.Message("Person full name was not loaded"));
+
+            return resp;
+            */
+        }
+
+        [HttpGet("{id}")]
+        public Task<IActionResult> Get(int id)
+        {
+            throw new NotImplementedException();
+            //return await new LifeEventModel(factory, mapper).GetItemAsync(new GetLifeEventRequest() { Id = id });
+            //return new JsonResult(answer);
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Post([FromBody]  UpdateLifeEventDto request)
+        {
+            throw new NotImplementedException();
+            //return await new LifeEventModel(factory, mapper).CreateAsync(request);
+        }
+
+        [HttpPut("{id}")]
+        public Task<IActionResult> Put(int id, [FromBody] UpdateLifeEventDto request)
+        {
+            throw new NotImplementedException();
+            //return await new LifeEventModel(factory, mapper).UpdateAsync(request);
+        }
+
+        [HttpDelete("{id}")]
+        public Task<IActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+            //return await new LifeEventModel(factory, mapper).DeleteAsync(new DeleteLifeEventRequest() { Id = id });
+        }
+    }
+}
---
+++ src/microservices/person/PersonDiary.Person.WebApi/Controllers/PersonController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using PersonDiary.Person.Dto;
+
+namespace PersonDiary.Person.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        public PersonController()
+        {
+        }
+        
+        [HttpGet]
+        public async Task<GetPersonsResponseDto> Get(string json)
+        {
+            var gateWayGetPersonsDto = JsonConvert.DeserializeObject<GetPersonsRequestDto>(json);
+            var getPersonDto = MapperPerson.GateWayGetPersonsDtoToPersonDto(gateWayGetPersonsDto);
+            var getPersonsResponseDto = await personApiClient.GetPersonsAsync(getPersonDto);
+
+            var gateWayGetPersonsResponseDto = MapperPerson.GetPersonsDtoToGateWayDto(getPersonsResponseDto);
+            return gateWayGetPersonsResponseDto;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<GetPersonResponseDto> Get(GateWayGetPersonRequestDto getPersonRequestDto)
+        {
+            var getPersonDto = MapperPerson.GateWayGetPersonDtoToPersonDto(getPersonRequestDto);
+            var result = await personApiClient.GetPersonAsync(getPersonDto);
+            
+            return result;
+        }
+        
+        [HttpPost]
+        public Task Post([FromBody]  UpdatePersonRequestDto request)
+        {
+            throw new NotImplementedException();
+            //return Ok(answer);
+        }
+        
+        [HttpPut]
+        public Task Put([FromBody] UpdatePersonRequestDto request)
+        {
+            throw new NotImplementedException();
+            //return Ok(answer);
+        }
+        
+        [HttpDelete]
+        public Task Delete(DeletePersonRequestDto deletePersonRequestDto)
+        {
+            throw new NotImplementedException();
+            //return Ok(answer);
+        }
+
+    }
+}
